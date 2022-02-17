91d37db2a4e69284700a23914d0f169b4d0a107e
elodin19 <joanoldaniel@gmail.com>
2022-02-17 14:09:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Write your name:");
+string name = Console.ReadLine();
+Console.WriteLine("What's up, " + name + "?");
+
+Console.WriteLine("It's " + DateTime.Now.ToString("hh:mm:ss tt"));
